a0570ddf40ddfc74985733af1e1bff35454ff33a
molinje <molinje@gmail.com>
2026-04-02 23:47:14 +0200

    adicionar primas electric 1

--- webapp/controller/electric.controller.js
+++ webapp/controller/electric.controller.js
@@ -39,11 +39,20 @@ sap.ui.define([
 				ValorPrestamoMelectric: 0,
 				ValorDescPrimasMelectric: 0,
 
+				// Descuento en primas
+				descuentoPrimas: "NO",
+
+				// Total primas a descontar
+				valorTotalPrimas: 0,
+
 				// Control del botón Crear
 				solicitudEnabled: true
 			});
 
 			this.getView().setModel(oViewModel, "movelectricView");
+
+			var oPrimasModel = new JSONModel({ items: [] });
+			this.getView().setModel(oPrimasModel, "listprimasElectric");
 
 			// Suscribirse al evento de ruta: se dispara cada vez que se navega a esta vista
 			var oRouter = this.getOwnerComponent().getRouter();
@@ -314,6 +323,77 @@ sap.ui.define([
 						{ title: "Error al guardar" }
 					);
 				});
+		},
+
+		/**
+		 * Agrega una prima consultando el servicio Add_PrimaService
+		 */
+		onAddPrimas: function () {
+			var oGlobalModel = this.getOwnerComponent().getModel("globalData");
+			var oUserData = oGlobalModel.getProperty("/userData");
+			var oPrestamoSeleccionado = oGlobalModel.getProperty("/prestamoSeleccionado");
+
+			var oViewModel = this.getView().getModel("movelectricView");
+			var fValorSolicitado = oViewModel.getProperty("/valorSolicitado");
+			var moneda = oViewModel.getProperty("/moneda");
+			var employeenumber = oUserData ? oUserData.PERNR : "";
+			var idPrestamo = oPrestamoSeleccionado ? oPrestamoSeleccionado.PrestamoId : "";
+
+			var oViewModelPrimas = this.getView().getModel("listprimasElectric");
+			var aPrimas = oViewModelPrimas.getProperty("/items") || [];
+			var NoPrimas = aPrimas.length + 1;
+
+			var dataPrima = {
+				"EMPLEADO": employeenumber,
+				"VALOR_PRESTAMO": String(fValorSolicitado),
+				"CANTIDAD_PRIMAS": String(NoPrimas),
+				"TIPO_PRESTAMO": idPrestamo,
+				"PORCENTAJE": "50"
+			};
+
+			this._oBackendService.Add_PrimaService(dataPrima)
+				.then(function (oResponse) {
+					var aItems = oResponse["n0:ZCOHCMF_PRIMAS_PRESTAMOSResponse"]
+						.RESPONSE_INFO_PRIMA.item;
+					if (!aItems) {
+						MessageToast.show("No se encontraron primas para los datos ingresados");
+						return;
+					}
+					if (!Array.isArray(aItems)) {
+						aItems = [aItems];
+					}
+
+					var fTotalPrimas = 0;
+					var iIdx = 0;
+					while (iIdx < aItems.length) {
+						var fValorPrima = parseFloat(aItems[iIdx].VALOR_PRIMA) || 0;
+						var iValorEntero = Math.trunc(fValorPrima * 100);
+						aItems[iIdx].VALOR_PRIMA = String(iValorEntero);
+						aItems[iIdx].MONEDA_PRIMA = moneda;
+						fTotalPrimas = fTotalPrimas + fValorPrima;
+						iIdx = iIdx + 1;
+					}
+					oViewModel.setProperty("/valorTotalPrimas", fTotalPrimas);
+					oViewModelPrimas.setProperty("/items", aItems);
+				})
+				.catch(function (oError) {
+					MessageBox.error(
+						"Error al consultar primas: " + (oError.message || oError.statusText || "Error desconocido"),
+						{ title: "Error" }
+					);
+				});
+		},
+
+		/**
+		 * Elimina el último registro de la tabla de primas
+		 */
+		onReducePrimas: function () {
+			var oViewModelPrimas = this.getView().getModel("listprimasElectric");
+			var aPrimas = oViewModelPrimas.getProperty("/items") || [];
+			if (aPrimas.length > 0) {
+				aPrimas.pop();
+				oViewModelPrimas.setProperty("/items", aPrimas);
+			}
 		},
 
 		onNavBack: function () {
